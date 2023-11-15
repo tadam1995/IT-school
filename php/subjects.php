0402a91aff778285fa74980359545b115344a38d
nemethtibormako <nemethtibor1975@gmail.com>
2023-11-15 22:23:28 +0100

    subjects.php elkészítése és a kepzes1.html átalakítása Angular JS-el

---
+++ php/subjects.php
@@ -0,0 +1,34 @@
+<?php
+
+// Set environment
+require_once('../../../common/php/environment.php');
+
+// Connect to database
+$db = new Database();
+
+// Set query
+$query = "  SELECT      courses.course_name,
+                        subjects.subject_name,
+                        subjects.description,
+                        CONCAT_WS(' ', teacher.surname, teacher.first_name) AS teacher_name
+                        FROM courses
+                        JOIN courses_subjects 
+                        ON courses.course_id = courses_subjects.course_id
+                        JOIN subjects 
+                        ON courses_subjects.subject_id = subjects.subject_id
+                        JOIN teacher 
+                        ON subjects.teacher_id = teacher.teacher_id;
+";
+
+// Execute query with argument
+$result = $db->execute($query);
+
+// Close connection
+$db = null;
+
+// Set response
+Util::setResponse($result);
+
+?>
+
+
